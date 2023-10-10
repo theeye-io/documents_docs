38ff9beaf43263bf65e854b8a4b6fbc99f5601f6
facugon <facugon@theeye.io>
2023-10-10 14:19:49 -0300

    docs: more info (#9)

---
+++ carga/convert_document.js
@@ -0,0 +1,68 @@
+const FormData = require('form-data')
+const path = require('path')
+const fs = require('fs')
+
+const https = require('https')
+const accessToken = process.env.TAGGER_ACCESS_TOKEN
+
+//url="https://tagger-api-dev.theeye.io/api/documents/upload?access_token=${accessToken}"
+//--file="@${path}"
+
+const main = async ([ filepath ]) => {
+  const content =  fs.createReadStream(filepath)
+
+  const formData = new FormData()
+  formData.append('file', content)
+
+  const request = https.request({
+    method: 'post',
+    host: 'tagger-api-dev.theeye.io',
+    path: `/api/documents/upload?access_token=${accessToken}`,
+    headers: formData.getHeaders()
+  })
+
+  formData.pipe(request)
+
+  request.on('response', res => {
+    const data = []
+
+    res.on('data', chunk => {
+      data.push(chunk)
+    })
+
+    return new Promise( (resolve, reject) => {
+      res.on('end', () => {
+        let payload;
+        const buffer = Buffer.concat(data).toString()
+
+        // assuming that the response is always JSON.
+        // But better would be to check the response content-type header
+        //if (/json/.test(res.headers['content-type'])) {
+        try {
+          payload = JSON.parse(buffer)
+        } catch (e) {
+          payload = buffer
+        }
+        //}
+        res.body = payload
+
+        if (res.statusCode >= 400) {
+          const err = new Error(`${res.statusCode}: ${res.body?.message||res.body}`)
+          err.response = res
+          err.request = req
+          reject(err)
+          return
+        }
+
+        // return the response object.
+        // assign the body to a response property body
+        console.log(res.statusCode)
+        console.log(res.body)
+        resolve(res)
+      })
+    })
+  })
+
+}
+
+main(process.argv.slice(2)).then(console.log).catch(console.error)
